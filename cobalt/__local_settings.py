a586550fdd1353ae806d537d652ded6d0a8e07bb
Krody Robert <krody.robi@gmail.com>
2016-04-12 14:42:55 +0300

    Added dependencies and mock api view / docs

---
+++ cobalt/__local_settings.py
@@ -0,0 +1,16 @@
+from .settings import *
+
+
+REST_FRAMEWORK_DOCS = {
+    'HIDE_DOCS': False
+}
+
+DEBUG = True
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
