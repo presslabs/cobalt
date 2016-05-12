322c7d2f808414d74a80b6bd16803837c30cc301
Krody Robert <krody.robi@gmail.com>
2016-05-12 16:01:26 +0300

    Fixed lease unit tests.

--- tests/unit/engine/test_lease.py
+++ tests/unit/engine/test_lease.py
@@ -5,11 +5,14 @@ from engine.lease import Lease
 
 class TestLease:
     def test_initialization(self, mocker):
-        lock = mocker.MagicMock(is_aquired=False)
-        lease = Lease(lock, 9)
+        lock = mocker.MagicMock(is_acquired=False)
+        lease = Lease(lock, 9, 10)
 
         assert lease.lock == lock
+        assert lease.lease_ttl == 10
+        assert lease.refresh_ttl == 20 / 3
         assert not lease.is_held
+        assert not lease.quit
 
     def test_acquire(self, mocker):
         lock = mocker.MagicMock()
@@ -22,9 +25,19 @@ class TestLease:
 
         mocker.patch('engine.lease.time.sleep', return_value=None)
 
-        lease = Lease(lock, 10)
+        lease = Lease(lock, 10, 6)
 
         with pytest.raises(RuntimeError):
             lease.acquire()
 
         assert lease.is_held
+
+    def test_quit_acquire(self, mocker):
+        lock = mocker.MagicMock(is_acquired=False)
+        lease = Lease(lock, 10, 6)
+
+        lease.quit = True
+        lease.acquire()
+
+        assert not lock.acquire.called
+        assert not lease.is_held
