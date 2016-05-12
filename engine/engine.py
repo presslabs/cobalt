76f43bbb8760f38436fb15245c0b42507b18da6f
Krody Robert <krody.robi@gmail.com>
2016-05-12 15:10:35 +0300

    Added gevents and a way to stop the engine service

--- engine/engine.py
+++ engine/engine.py
@@ -1,4 +1,5 @@
 import gevent
+import time
 
 from engine.lease import Lease
 from utils.service import Service
@@ -12,7 +13,10 @@ class Engine(Service):
 
         self.leaser_loop = None
         self.runner_loop = None
+        self.should_stop = False
+
         self._started = False
+        self.quit = False
 
     def start(self):
         if self._started:
@@ -21,18 +25,24 @@ class Engine(Service):
         self.leaser_loop = gevent.spawn(self.lease.acquire)
         self.runner_loop = gevent.spawn(self._run)
 
-        gevent.joinall([self.runner_loop, self.leaser_loop], wait=0.0)
         self._started = True
+        return [self.runner_loop, self.leaser_loop]
 
     def stop(self):
         if not self._started:
             return
 
-        gevent.killall([self.runner_loop, self.leaser_loop], block=False)
-        self._started = False
+        self.lease.quit = True
+        self.quit = True
 
     def _run(self):
-        pass
+        while not self.quit:
+            if not self.lease.is_held:
+                # TODO read from config
+                time.sleep(10)
+                continue
 
+            # required for yield so other coroutines can function
+            time.sleep(10)
 
     # http://stackoverflow.com/a/13602029
--- engine/lease.py
+++ engine/lease.py
@@ -2,18 +2,23 @@ import time
 
 
 class Lease(object):
-    def __init__(self, lock, lease_ttl=10):
+    def __init__(self, lock, lease_ttl=60, refresh_ttl=40):
         self.lock = lock
-        self.lock.release()
 
         self.lease_ttl = 10 if lease_ttl < 10 else lease_ttl
+        self.refresh_ttl = 6 if refresh_ttl < 6 else refresh_ttl
+
+        if self.refresh_ttl >= self.lease_ttl:
+            self.refresh_ttl = 2 * self.lease_ttl / 3
+
+        self.quit = False
 
     def acquire(self):
-        while True:
-            self.lock.acquire(lock_ttl=self.lease_ttl)
+        while not self.quit:
+            self.lock.acquire(lock_ttl=self.lease_ttl, timeout=0)
 
-            time.sleep(self.lease_ttl * 2 / 3)
+            time.sleep(self.refresh_ttl)
 
     @property
     def is_held(self):
-        return self.lock.is_aquired
+        return self.lock.is_acquired
