b7d42ee006c558b6ea30ff38e544c4364fe855d9
Krody Robert <krody.robi@gmail.com>
2016-05-18 18:00:14 +0300

    Fixed ordering issue with flask-restfull

--- api/app.py
+++ api/app.py
@@ -7,6 +7,11 @@ app = Flask(__name__)
 api = RestApi(app)
 
 app.debug = True
+app.config['RESTFUL_JSON'] = {
+    'separators': (', ', ': '),
+    'indent': 2,
+    'sort_keys': False
+}
 
 api.add_resource(VolumeList, '/volumes')
 api.add_resource(Volume, '/volumes/<volume_id>')
