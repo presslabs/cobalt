8613644a84ac9687f235cdc8f54de088138bbac3
Krody Robert <krody.robi@gmail.com>
2016-05-25 14:39:08 +0300

    start/stop uniform responses, now using Connection + util import cleanup

--- api/api.py
+++ api/api.py
@@ -1,25 +1,22 @@
 import gevent
 
+from flask import Flask
 from gevent.pywsgi import WSGIServer
 from werkzeug.debug import DebuggedApplication
 
-from utils.service import Service
-from api.app import app, api
-from api.volume import register_resources
+from utils import Service, Connection
 
 
 class Api(Service):
-    def __init__(self, volume_manager, host='', port=5000):
-        self._api_server = WSGIServer((host, port), DebuggedApplication(app))
-        app.volume_manager = volume_manager
-        register_resources(api)
+    def __init__(self, app: Flask, connection: Connection=Connection('', 5000)):
+        self._api_server = WSGIServer(connection, DebuggedApplication(app))
 
         self._api_loop = None
         self._started = False
 
     def start(self):
         if self._started:
-            return
+            return False
 
         self._started = True
         self._api_loop = gevent.spawn(self._api_server.serve_forever)
@@ -28,7 +25,9 @@ class Api(Service):
 
     def stop(self):
         if not self._started:
-            return
+            return False
 
         self._started = False
         self._api_server.stop()
+
+        return True
