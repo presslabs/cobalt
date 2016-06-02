6427759999c7b0749c1dbd07a60fb64e1c117cc1
Krody Robert <krody.robi@gmail.com>
2016-06-02 10:10:24 +0300

    Added executor integration test skeleton, and finalized the executor?

--- engine/executor.py
+++ engine/executor.py
@@ -35,26 +35,86 @@ class Executor:
         if self._volumes_to_process:
             volume = self._volumes_to_process.pop()
         else:
+            # TODO expose timeout here as an extra config
             volume = self.volume_manager.watch(timeout=self.delay, index=self._watch_index)
             if volume is None:
                 self.reset()
                 return
 
             self._watch_index = volume.modifiedIndex + 1
+            if volume.action == 'expire' or volume.action == 'delete':
+                return
 
         self._process(volume)
 
     def _process(self, volume):
         in_state = self.volume_manager.filter_states([volume], self.states_interested_in)
-        # make check to see if worth working on volume short-circuit
+        if not in_state:
+            return
 
-        # machines = self.machine_manager.all()
+        data = volume.value
+        state = data['state']
+        node = data['node']
 
-        # remove machines without labels
-        # sort machines by available space
+        # TODO expose expired timeout in config
+        expired = True if time.time() - data['control']['updated'] > self.delay else False
 
-        pass
+        if state == 'scheduling':
+            if not node or expired:
+                machine = self._find_machine(volume)
+                if not machine:
+                    return
+
+                data['node'] = machine.value['name']
+        elif state == 'pending':
+            if data['requested'] == data['actual']:
+                data['state'] = 'ready'
+            else:
+                next_state = self._next_state(volume)
+                if not next_state:
+                    return
+
+                data['state'] = next_state
+                if next_state == 'cloning':
+                    parent = self.volume_manager.by_id(data['control']['parent_id'])
+                    data['node'] = parent.value['node']
+
+        self.volume_manager.update(volume)
 
     # TODO test volume_manager watch
     # TODO test executor
     # TODO test engine _machine loop
+    # TODO test api delete when clones exist
+
+    def _find_machine(self, volume):
+        constraints = volume.value['requested']['contraints']
+        machines = self.machine_manager.all()[1]
+
+        machines_ok = []
+        for machine in machines:
+            labels = machine.value['labels']
+            if not all(x in labels for x in constraints):
+                continue
+
+            if machine.value['available'] < volume.value['requested']['reserved_size']:
+                continue
+
+            machines_ok.append(machine)
+
+        if not machines_ok:
+            return
+
+        machines_ok.sort(key=lambda x: x.value['available'], reverse=True)
+        return machines_ok[0]
+
+    def _next_state(self, volume):
+        value = volume.value
+
+        if value['control']['parent_id']:
+            return 'cloning'
+
+        if value['requested']['reserved_size'] != volume['actual']['reserved_size']:
+            return 'resizing'
+
+        print('Next state for volume {} can\'t be determined!'.format(volume))
+        return
---
+++ tests/integration/engine/test_executor.py
@@ -0,0 +1,49 @@
+
+class TestExecutor:
+    def test_scheduling_no_machines(self):
+        pass
+
+    def test_scheduling_1_machine_labels_mismatch(self):
+        pass
+
+    def test_scheduling_1_machine_no_room(self):
+        pass
+
+    def test_scheduling_2_machines_1_labels_mismatch_other_no_room(self):
+        pass
+
+    def test_scheduling_2_machines_1_invalid_1_ok(self):
+        pass
+
+    def test_scheduling_2_machines_prioritize_largest(self):
+        pass
+
+    def test_scheduling_machine_away_no_other_machine(self):
+        pass
+
+    def test_scheduling_machine_away_1_other_available(self):
+        pass
+
+    def test_not_interested_state(self):
+        pass
+
+    def test_pending_no_changes(self):
+        pass
+
+    def test_pending_to_cloning(self):
+        pass
+
+    def test_pending_to_resizing(self):
+        pass
+
+    def test_process_with_watch_ignore_ttl_deleting(self):
+        pass
+
+    def test_process_with_watch(self):
+        pass
+
+    def test_process_with_multiple_consecutive_watches(self):
+        pass
+
+    def test_process_with_multiple_watches_with_reset_between(self):
+        pass
