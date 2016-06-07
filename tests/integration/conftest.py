2dffd1739151ebac405145d7c2b0fa9afede6835
Krody Robert <krody.robi@gmail.com>
2016-06-07 11:36:02 +0300

    Added version checks. Fixes #91

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -42,7 +42,7 @@ def etcd_client(request):
     client = etcd.Client(**context['etcd'])
 
     def fin():
-        entrypoints = [VolumeManager.KEY, '_locks', 'machines']
+        entrypoints = [VolumeManager.KEY, '_locks', 'machines', 'version']
 
         for entry in entrypoints:
             try:
