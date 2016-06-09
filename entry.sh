9668cef2215e6a4646cb41358d07b8ecea13abb5
Krody Robert <krody.robi@gmail.com>
2016-06-09 16:06:23 +0300

    Fixed term glitches

--- entry.sh
+++ entry.sh
@@ -1,5 +1,14 @@
-#!/bin/bash
+#!/bin/sh
+
+clean_up() {
+	umount /mnt
+	kill -TERM "$child" 2>/dev/null
+}
+trap clean_up SIGHUP SIGINT SIGTERM
+
 
 mount /btrfs-volume /mnt
+python src/main.py "/etc/cobalt/config.json" &
 
-python src/main.py "/etc/cobalt/config.json"
+child=$!
+wait "$child"
